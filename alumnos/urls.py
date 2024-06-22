f66bbf6ed974d22d20ffda09f3109aaafcb9ae86
morales.inzulza.matias@gmail.com <morales.inzulza.matias@gmail.com>
2024-06-22 00:43:20 -0400

    Agregar redirige pero arroja error message

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, crud
+from alumnos import views
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('crud', views.crud, name='crud'),
+    path('alumnos/alumnosAdd', views.alumnosAdd, name='alumnosAdd'),
 ]
--- alumnos/views.py
+++ alumnos/views.py
@@ -20,3 +20,49 @@ def index(request):
     context={"hijo":hijo, "nombre":"Claudia Andrea", "comidas":lista, "alumnos":alumnos }
 
     return render(request, 'alumnos/index.html', context)
+
+def crud(request):
+    alumnos = Alumno.objects.all()
+    context = {'alumnos': alumnos}
+    return render(request, 'alumnos/alumnos_list.html', context)
+
+def alumnosAdd(request):
+        if request.method != "POST":
+            #no es un POST, por lo tanto se muestra el formulario para agregar
+            generos=Genero.objects.all()
+            context={'generos' :generos}
+            return render(request, 'alumnos/alumno_add.html', context)
+        elif request.method == "POST":
+            #Es un POST, por lo tanto se recuperan los datos del formulario y se graban en la tabla.
+
+            rut=request.POST["rut"]
+            nombre=request.POST["nombre"]
+            aPaterno=request.POST["paterno"]
+            aMaterno=request.POST["materno"]
+            fechaNac=request.POST["fechaNac"]
+            genero=request.POST["genero"]
+            telefono=request.POST["telefono"]
+            email=request.POST["email"]
+            direccion=request.POST["direccion"]
+            activo="1"
+
+            print("Datos recibidos del formulario:")
+            print(f"Rut: {rut}, Nombre: {nombre}, Apellido Paterno: {aPaterno}, Apellido Materno: {aMaterno}, Fecha Nacimiento: {fechaNac}, Género: {genero}, Teléfono: {telefono}, Email: {email}, Dirección: {direccion}")
+
+            objGenero=Genero.objects.get(id_genero = genero)
+            obj=Alumno.objects.create(  rut=rut,
+                                        nombre=nombre,
+                                        apellido_paterno=aPaterno,
+                                        apellido_materno=aMaterno,
+                                        fecha_nacimiento=fechaNac,
+                                        id_genero=objGenero,
+                                        telefono=telefono,
+                                        email=email,
+                                        direccion=direccion,
+                                        activo=1)
+            print("Objeto Alumno creado:", obj)
+            obj.save()
+            print("Datos guardados en la base de datos")
+            context['mensaje'] = "Ok, datos guardados..."
+            return render(request, 'alumnos/alumno_add.html', context)
+        
